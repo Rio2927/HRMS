807673f7d0e77a2e54a09d39387e4c90a919d52e
Rohit Oberoi <rio.oberoi.2704@gmail.com>
2025-10-13 00:29:33 +0530

    added portals

--- frontend/src/navbar.jsx
+++ frontend/src/navbar.jsx
@@ -57,7 +57,7 @@ function Navbar() {
                 </IconButton> */}
 
         <Typography variant="h6" noWrap sx={{ flexGrow: 1 }}>
-          HRMS Portals hh                                                                 {/* Idhar kiya hai pipeline test change */}
+          HRMS Portals                                                                  {/* Idhar kiya hai pipeline test change */}
         </Typography>
 
         <Box>
